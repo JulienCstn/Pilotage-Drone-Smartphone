43ce1c5b1e80860abc9055429ae3eb2f6ff4b440
Syfalko <44257279+Syfalko@users.noreply.github.com>
2020-04-02 19:04:13 +0200

    Utilisation du code de base + mise place de la barre de nav entre les différents fragments + affichage des valeurs de l'accélérometre (manual driving) + ajout des cartes et marqeurs (automatic driving et checkpoint)

--- Application/app/src/main/java/com/ulr/dronemanager/ui/automaticDriving/AutomaticDrivingFragment.java
+++ Application/app/src/main/java/com/ulr/dronemanager/ui/automaticDriving/AutomaticDrivingFragment.java
@@ -9,13 +9,23 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.ulr.dronemanager.R;
 
-public class AutomaticDrivingFragment extends Fragment {
+public class AutomaticDrivingFragment extends Fragment implements OnMapReadyCallback {
     private AutomaticDrivingViewModel automaticDrivingViewModel;
+    SupportMapFragment supportMapFragment;
+    private GoogleMap mMapA;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -23,14 +33,32 @@ public class AutomaticDrivingFragment extends Fragment {
         automaticDrivingViewModel = ViewModelProviders.of(this).get(AutomaticDrivingViewModel.class);
         View root = inflater.inflate(R.layout.fragment_automatic_driving, container, false);
 
-        final TextView textView = root.findViewById(R.id.text_automaticDriving);
-        automaticDrivingViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+        /*final TextView textView = root.findViewById(R.id.text_checkpointPath);
+        checkpointPathViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
                 textView.setText(s);
             }
-        });
+        });*/
+        supportMapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
+        if(supportMapFragment == null){
+
+            FragmentManager fragmentManager = getFragmentManager();
+            FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+            supportMapFragment = SupportMapFragment.newInstance();
+            fragmentTransaction.replace(R.id.map, supportMapFragment).commit();
+        }
+        supportMapFragment.getMapAsync(this);
 
         return root;
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMapA = googleMap;
+        // Add a marker in larochelle, France, and move the camera.
+        LatLng larochelle = new LatLng(46.144152,  -1.166624);
+        mMapA.addMarker(new MarkerOptions().position(larochelle).title("Marker in LaRochelle"));//definir un marqueur
+        mMapA.moveCamera(CameraUpdateFactory.newLatLng(larochelle)); //envoyer la camera Ã  la position
     }
 }
--- Application/app/src/main/java/com/ulr/dronemanager/ui/checkpointPath/CheckpointPathFragment.java
+++ Application/app/src/main/java/com/ulr/dronemanager/ui/checkpointPath/CheckpointPathFragment.java
@@ -9,26 +9,69 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.ulr.dronemanager.R;
 
-public class CheckpointPathFragment extends Fragment {
+public class CheckpointPathFragment extends Fragment implements OnMapReadyCallback {
     private CheckpointPathViewModel checkpointPathViewModel;
+    SupportMapFragment supportMapFragment;
+    private GoogleMap mMap;
+    private Marker marker;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        checkpointPathViewModel =
-                ViewModelProviders.of(this).get(CheckpointPathViewModel.class);
+        super.onCreateView(inflater, container, savedInstanceState);
+        checkpointPathViewModel = ViewModelProviders.of(this).get(CheckpointPathViewModel.class);
         View root = inflater.inflate(R.layout.fragment_checkpoint_path, container, false);
-        final TextView textView = root.findViewById(R.id.text_checkpointPath);
+        /*final TextView textView = root.findViewById(R.id.text_checkpointPath);
         checkpointPathViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
                 textView.setText(s);
             }
-        });
+        });*/
+        supportMapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
+        if(supportMapFragment == null){
+
+            FragmentManager fragmentManager = getFragmentManager();
+            FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+            supportMapFragment = SupportMapFragment.newInstance();
+            fragmentTransaction.replace(R.id.map, supportMapFragment).commit();
+        }
+        supportMapFragment.getMapAsync(this);
+
+
         return root;
     }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        // Add a marker in larochelle, France, and move the camera.
+        LatLng larochelle = new LatLng(46.144152,  -1.166624);
+        mMap.addMarker(new MarkerOptions().position(larochelle).title("Marker in LaRochelle"));//definir un marqueur
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(larochelle)); //envoyer la camera Ã  la position
+        mMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+            @Override
+            public void onMapClick(LatLng point) {
+                MarkerOptions marker = new MarkerOptions().position(new LatLng(point.latitude,point.longitude)).title("New Marker");
+                mMap.addMarker(marker);
+                System.out.println(marker.getPosition());// donne la position du point créé
+            }
+        });
+
+    }
+
 }
