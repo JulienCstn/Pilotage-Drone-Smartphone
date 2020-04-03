0233788c0b952144730128a711e80784266b6b43
ThePositronCode <40057408+ThePositronCode@users.noreply.github.com>
2020-04-03 10:59:04 +0200

    AutomaticDrivingActivity v1.0 + Path + Waypoint

--- Application/app/src/main/java/com/ulr/dronemanager/AutomaticDrivingActivity.java
+++ Application/app/src/main/java/com/ulr/dronemanager/AutomaticDrivingActivity.java
@@ -1,5 +1,47 @@
 package com.ulr.dronemanager;
+import java.util.*;
+
+import java.util.Collection;
 
 public class AutomaticDrivingActivity {
-    
+    private DroneNetwork myDroneNetwork;
+    private Path myPath;
+
+    public AutomaticDrivingActivity(DroneNetwork myDroneNetwork, Path myPath)
+        this.myDroneNetwork = myDroneNetwork;
+        this.myPath = myPath;
+    }
+
+    public String getInformation(){
+        return myDroneNetwork.getEnvironemental();
+    }
+
+    private boolean goBackToFirstPoint(){
+        //Reverse our path
+        Collections.reverse(myPath.getWaypoints());
+        //Send new path to the drone
+        myDroneNetwork.sendPath(mypath);
+        return true;
+    }
+
+    public DroneNetwork getMyDroneNetwork() {
+        return myDroneNetwork;
+    }
+
+    public Path getMyPath() {
+        return myPath;
+    }
+
+    public void setMyDroneNetwork(DroneNetwork myDroneNetwork) {
+        this.myDroneNetwork = myDroneNetwork;
+    }
+
+    public void setMyPath(Path myPath) {
+        this.myPath = myPath;
+    }
+
+    @Override
+    public String toString() {
+        return "AutomaticDrivingActivity{" + "myDroneNetwork=" + myDroneNetwork + ", myPath=" + myPath +'}';
+    }
 }
